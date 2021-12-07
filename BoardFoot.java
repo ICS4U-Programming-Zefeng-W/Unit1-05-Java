6d7ae40d7056542339752bd0a7db1f2e45669d39
Ubuntu <ubuntu@ip-172-31-95-119.ec2.internal>
2021-12-07 03:52:09 +0000

    fixed comments

--- BoardFoot.java
+++ BoardFoot.java
@@ -20,6 +20,7 @@ class BoardFoot {
     double height = 0;
     double length = 0;
     while (true) {
+      // Ask the user for the width and height of the board foot and reads them
       try {
         System.out.println("Please enter the width: ");
         width = Double.parseDouble(scannerObj.next());
@@ -31,11 +32,12 @@ class BoardFoot {
         System.out.println("Please enter a number.");
       }
     }
+    // Display the length
     System.out.printf("Length is %s inch(es),height is %s inch(es), and width is %s inch(es)\n", 
                      length, height, width);
   }
 
-  // Calculates the length given the width and height
+  // Calculates the length of a board foot given its width and height
   public static double calculateBoardFoot(double width, double height) {
     final int boardFootVol = 144;
     return boardFootVol / (width * height);
